4dff4c3025b630753fe8e95278e94dd396c51576
Jose Alexis Correa <88249013+codehiggs@users.noreply.github.com>
2024-09-09 22:17:52 -0500

    Actualizacion 20000

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,7 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Bienvenidos y se sincronizo por sexta vez <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>XXX <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
